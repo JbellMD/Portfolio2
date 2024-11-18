e632375206c62454e52ce2871d0b1f16a764ff53
JbellMD <91991220+JbellMD@users.noreply.github.com>
2024-11-18 06:19:57 -0500

    Update CodeProjects.jsx
    Added a longer summary to each project

--- frontend/src/pages/CodeProjects.jsx
+++ frontend/src/pages/CodeProjects.jsx
@@ -3,26 +3,33 @@ import ProjectCard from '../components/ProjectCard';
 
 const projectData = [
     {
+        title: 'Weather Dashboard',
+        description: 'Created with API integration with OpenWeather to show standard web integration created with JavaScript and React demonstrating REST API principles.',
+        imageUrl: '/weather.png', // Replace with the actual path to a screenshot
+        demoLink: 'https://jbellmd.github.io/weather-dashboard/',
+        codeLink: 'https://github.com/JbellMD/weather-dashboard'
+    },
+    {
+        title: 'e-Commerce Wesbite: Plants',
+        description: 'This is my React powered e-Commerce wesbite to demonstrate the solution to a number of user stories of feature additions to their application',
+        imageUrl: '/plants.png',
+        demoLink: 'https://jbellmd.github.io/ReactApp/',
+        codeLink: 'https://github.com/JbellMD/ReactApp/'
+    },
+    {
+        title: 'Expense Calculator',
+        description: 'This expense calculator is a basic exercise for creating interactive frontends with JS while creating backend logic with Python',
+        imageUrl: '/arc1.png', // Replace with the actual path to a screenshot
+        demoLink: 'https://jbellmd.github.io/weather-dashboard/',
+        codeLink: 'https://github.com/JbellMD/weather-dashboard'
+    },
+     {
         title: 'Car Dealership Website',
         description: 'This my React powered Car Dealership website.',
         imageUrl: '/car.jpg', // Replace with the actual path to a screenshot
         demoLink: 'https://link-to-your-live-demo1.com',
         codeLink: 'https://github.com/JbellMD/ReactApp/'
     },
-    {
-        title: 'e-Commerce Wesbite: Plants',
-        description: 'This is my React powered e-Commerce wesbite',
-        imageUrl: '/plants.png',
-        demoLink: 'https://jbellmd.github.io/ReactApp/',
-        codeLink: 'https://github.com/JbellMD/ReactApp/'
-    },
-    {
-        title: 'Weather Dashboard',
-        description: 'Created with API integration to OpenWeather',
-        imageUrl: '/weather.png', // Replace with the actual path to a screenshot
-        demoLink: 'https://jbellmd.github.io/weather-dashboard/',
-        codeLink: 'https://github.com/JbellMD/weather-dashboard'
-    }
 ];
 
 const Projects = () => (
