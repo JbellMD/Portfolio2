4ac3e4f3162356ea693b0bee5675c621c56b55c9
Jonathan Bello <ijbello29@gmail.com>
2024-12-18 10:58:41 -0500

    Changed Portolio page

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import ProjectCard from '../components/ProjectCard';
 
 const projectData = [
-     {
+    {
+        title: 'AI Fitness Tracker',
+        description: 'This fitness tracker uses interactive frontends with React while creating backend logic with Firebase as a DB and featuring Firebase and OpenAI insights.',
+        imageUrl: '/fitness.png', // Replace with the actual path to a screenshot
+        demoLink: 'https://ai-ftiness-tracker.netlify.app/',
+        codeLink: 'https://github.com/JbellMD/ai-fitness-tracker'
+    },
+    {
         title: 'Weather Dashboard',
         description: 'Created with API integration with OpenWeather to show standard web integration created with JavaScript and React demonstrating REST API principles.',
         imageUrl: '/weather.png', // Replace with the actual path to a screenshot
@@ -17,15 +24,22 @@ const projectData = [
         codeLink: 'https://github.com/JbellMD/ReactApp/'
     },
     {
-        title: 'Expense Calculator',
-        description: 'This expense calculator is a basic exercise for creating interactive frontends with JS while creating backend logic with Python',
-        imageUrl: '/arc1.png', // Replace with the actual path to a screenshot
-        demoLink: 'https://jbellmd.github.io/weather-dashboard/',
-        codeLink: 'https://github.com/JbellMD/weather-dashboard'
+        title: 'Car Dealership Website v2',
+        description: 'This my React powered Car Dealership website featuring Django, MongoDB, and IBMs Sentiment Analyzer .',
+        imageUrl: '/car.jpg', // Replace with the actual path to a screenshot
+        demoLink: 'https://link-to-your-live-demo1.com',
+        codeLink: 'https://github.com/JbellMD/ReactApp/'
     },
-     {
+    {
+        title: 'AI Fitness Insights',
+        description: 'This application uses interactive frontends with React while creating backend logic with Firebase as a DB and featuring Firebase and OpenAI insights.',
+        imageUrl: '/fitness.png', // Replace with the actual path to a screenshot
+        demoLink: 'https://ai-ftiness-tracker.netlify.app/',
+        codeLink: 'https://github.com/JbellMD/ai-fitness-tracker'
+    },
+    {
         title: 'Car Dealership Website',
-        description: 'This my React powered Car Dealership website.',
+        description: 'This my React powered Car Dealership website featuring Django, MongoDB, and IBMs Sentiment Analyzer .',
         imageUrl: '/car.jpg', // Replace with the actual path to a screenshot
         demoLink: 'https://link-to-your-live-demo1.com',
         codeLink: 'https://github.com/JbellMD/ReactApp/'
@@ -44,3 +58,4 @@ const Projects = () => (
 );
 
 export default Projects;
+
